c6e014f476d3cf19ec781cc02e6bded174586f01
ogbestseller01 <ogbestseller01@gmail.com>
2026-08-31 20:29:58 +0300

    Clean root directory and retain react_frontend folder only

--- src/types/product.ts
+++
@@ -1,2 +0,0 @@
-// Placeholder for product types
-export interface Product {}; export interface ProductFormData {}; export interface ProductFilters {}; export interface ProductCategory {}; export interface Shop {}; export interface Company {};
